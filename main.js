3e45445c283b76662c99fd0495c7c41717a9b270
Alexander <vealexander.gaming73@gmail.com>
2026-03-18 14:36:40 +0100

    Estructura inicial: añadida página de prueba con botones y estilos + borrado de archivos que no voy a usar

--- main.js
+++
@@ -1,8 +0,0 @@
-import { writeFile } from "fs/promises";
-import { render } from "./render.js";
-import { loadUsers } from "./users.js";
-
-const users = await loadUsers(100);
-const html = render(users);
-await writeFile('users.html', html);
-
---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarAlerta1() {
+    alert("¡Hola! Has pulsado el botón principal.");
+}
+
+function mostrarAlerta2() {
+    alert("¡Funciona! Este es el mensaje del segundo botón.");
+}
